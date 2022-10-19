249a7b5c5093a2ea56bec6483172a71b600e33b5
sdaqo <sdaqo.dev@protonmail.com>
2022-10-19 13:26:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import sqlite3
 from flask import Flask
+from pathlib import Path
+
+
+app = Flask.
+def init_db(file_path: Path):
+  con = sqlite3.connect(file_path.__str__())
+  return con.cursor()
