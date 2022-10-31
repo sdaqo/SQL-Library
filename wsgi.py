8cb6800d4d97c02aa63e0439e5b95da710a86cfc
sdaqo <sdaqo.dev@protonmail.com>
2022-10-31 23:46:58 +0100

    dont debug in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from library_db.logger import RequestHandler
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", request_handler=RequestHandler, debug=True)
+    app.run(request_handler=RequestHandler)
